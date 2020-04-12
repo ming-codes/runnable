a715dcc50a4c3df1b3997edb5ca2d6233a6e7cc2
Ming Liu <ming.codes@gmail.com>
2020-04-12 12:40:51 -0700

    wip;

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const TASK_STACK = [];
 
-const COMPLETE_STATE = 1;
-const ERROR_STATE = 2;
-
-let parent = null;
+const COMPLETE_STATE = Symbol('complete');
+const ERROR_STATE = Symbol('error');
+const CANCEL_TOKEN = Symbol('cancel');
 
 export class Runnable {
   constructor(generator) {
@@ -16,138 +15,146 @@ export class Runnable {
 }
 
 export class Task {
-  static canceled = null;
-
   constructor(iterator = null) {
-    //const parent = TASK_STACK[TASK_STACK.length - 1] || null;
+    this.parent = TASK_STACK[TASK_STACK.length - 1] || null;
 
     this.iterator = iterator;
-    //this.children = [];
+    this.children = new Set();
 
-    //if (parent) {
-    //  parent.children.push(this);
-    //}
+    if (this.parent) {
+      this.parent.children.add(this);
+    }
+
+    this.promise = new Promise((resolve, reject) => {
+      this.resolve = resolve;
+      this.reject = reject;
+    });
 
     this.proceed(null);
   }
 
-  step(value) {
+  then(onResolve, onReject) {
+    return this.promise.then(onResolve, onReject);
+  }
+
+  /** @private */
+  tap(method, value) {
     try {
-      return this.iterator.next(value);
+      TASK_STACK.push(this);
+      return this.iterator[method](value);
+    } finally {
+      TASK_STACK.pop();
+    }
+  }
+
+  /** @private */
+  step(value, done, next) {
+    try {
+      if (value === CANCEL_TOKEN) {
+        next(this.tap('return', value));
+      } else if (value && typeof value.then === 'function') {
+        value.then(
+          (resolved => {
+            this.step(resolved, done, next);
+          }),
+          (rejected => {
+            try {
+              next(this.tap('throw', rejected));
+            } catch (error) {
+              next({
+                error: true,
+                value: error
+              });
+            }
+          }),
+        );
+      } else if (done) {
+        next({ value });
+      } else {
+        next(this.tap('next', value));
+      }
     } catch (error) {
-      return {
+      next({
         error: true,
         value: error
-      };
+      });
     }
   }
 
+  /** @private */
   proceed(previous) {
-    const { value, done, error } = this.step(previous);
-
-    if (error) {
-      this.throw(value);
-    } else if (done) {
-      this.finalize(value);
-    } else {
-      this.proceed(value);
-    }
+    this.step(previous, false, ({ value, done, error }) => {
+      if (error) {
+        this.error(value);
+      } else if (done) {
+        this.complete(value);
+      } else {
+        this.proceed(value);
+      }
+    });
   }
 
-  throw(previous) {
-    this.state = ERROR_STATE;
-    this.value = previous;
+  /** @private */
+  error(previous) {
+    this.step(previous, true, ({ value }) => {
+      this.state = ERROR_STATE;
+      this.value = value;
+
+      this.reject(value);
+
+      if (this.parent) {
+        this.parent.children.delete(this);
+        this.parent = null;
+      }
+    });
   }
 
-  finalize(previous) {
-    this.state = COMPLETE_STATE;
-    this.value = previous;
+  /** @private */
+  complete(previous) {
+    this.step(previous, true, ({ value }) => {
+      this.state = COMPLETE_STATE;
+      this.value = value;
+
+      this.resolve(value);
+
+      if (this.parent) {
+        this.parent.children.delete(this);
+        this.parent = null;
+      }
+    });
   }
 
-  //proceed(previous) {
-  //  debugger;
-  //  if (previous instanceof Task) {
-  //    // do nothing?
-  //    debugger;
-  //  } if (previous && typeof previous.then === 'function') {
-  //    previous
-  //      .then(
-  //        value => {
-  //          this.proceed(value);
-  //        },
-  //        reason => {
-  //          this.throw(reason);
-  //        }
-  //      );
-  //  } else {
-  //    try {
-  //      TASK_STACK.push(this);
-  //      const { value, done } = this.iterator.next(previous);
-  //      TASK_STACK.pop();
+  //
+  // It's not a cancel.
+  // Cancel implies the iterator goes into a terminal state.
+  // But it doesn't. You can't force a generator to go into
+  // a terminal state. The generator has to choose to go into
+  // a terminal state.
+  interrupt() {
+    this.proceed(CANCEL_TOKEN);
 
-  //      if (done) {
-  //        this.finalize(value);
-  //      } else {
-  //        this.proceed(value);
-  //      }
-  //    } catch (error) {
-  //      this.throw(error);
-  //    }
-  //  }
-  //}
-
-  //throw(error) {
-  //  debugger;
-  //}
-
-  //finalize(value) {
-  //  debugger;
-  //  if (value && typeof value.then === 'function') {
-  //    value.then(value => {
-  //      this.finalize(value);
-  //    });
-  //  } else {
-  //    // TODO notify done
-  //    debugger;
-  //  }
-  //}
-
-  ////async start() {
-  ////  let value = null;
-  ////  let done = false;
-
-  ////  do {
-  ////    try {
-  ////      const prev = parent;
-
-  ////      parent = this.children;
-
-  ////      if (value instanceof Task) {
-  ////        value = await value.promise;
-  ////      } else {
-  ////        value = await value;
-  ////      }
-  ////      // TODO there's some problem with waiting for another task
-  ////      ({ value, done } = this.iterator.next(value));
-  ////      parent = prev;
-
-  ////      // TODO apparently parent/child linking is a big problem in EC too 
-  ////      //TASK_STACK.slice(TASK_STACK.indexOf(this)); // This is descendents, not just children
-  ////                                                  // The stack is really a tree
-  ////    } finally {
-  ////    }
-
-  ////  } while (!done);
-
-  ////  return value;
-  ////}
-
-  //cancel() {
-  //  Task.canceled = this;
+    return;
+    //Task.canceled = this;
   //  this.children.forEach(child => {
   //    child.cancel();
   //  });
-  //  this.iterator.return();
-  //  Task.canceled = null;
-  //}
+    debugger;
+    // XXX
+    // Cancel is a special case.
+    // .return calls into finally block, but the block is still allowed to throw
+    try {
+      const cancel = Symbol();
+      const { value, done } = this.iterator.return(cancel);
+      // TODO
+      // - [ ] it can throw
+      // - [ ] it can return ""
+      // - [ ] it can yield and just refuse to terminate
+      //
+      // So it's like a interrupt signal. The process can choose not to shut down.
+      debugger;
+    } catch (error) {
+      debugger;
+    }
+    //Task.canceled = null;
+  }
 }
--- src/index.test.js
+++ src/index.test.js
@@ -13,12 +13,27 @@ function marble(expr, fn) {
       stream.push(ch);
     });
 
-    if (init instanceof Runnable) {
-      const { value, state } = await init.run();
+    if (init instanceof Task) {
+      const task = init;
 
-      switch (state) {
-        case 1: stream.push('|', value); break;
-        case 2: stream.push('#', value); break;
+      try {
+        await task;
+
+        stream.push('|', task.value);
+      } catch (error) {
+        stream.push('#', task.value);
+      }
+    }
+
+    if (init instanceof Runnable) {
+      const task = init.run();
+
+      try {
+        await task;
+
+        stream.push('|', task.value);
+      } catch (error) {
+        stream.push('#', task.value);
       }
     }
 
@@ -60,6 +75,22 @@ describe(Runnable.name, function() {
       });
     }));
 
+    it('should allow thrown errors to be caught', marble('ab|d', function(emit) {
+      return new Runnable(function* () {
+        emit('a');
+
+        yield 1;
+
+        emit('b');
+
+        try {
+          throw 'c';
+        } catch (error) {
+          return 'd';
+        }
+      });
+    }));
+
     it('should run with child', marble('a123b123c|d', function(emit) {
       const child = new Runnable(function* () {
         emit('1');
@@ -89,8 +120,8 @@ describe(Runnable.name, function() {
     }));
   });
 
-  describe.only('asynchronous usage', function() {
-    it('should run async task', marble('abc|d', async function(emit) {
+  describe('asynchronous usage', function() {
+    it('should run with simple yield', marble('abc|d', function(emit) {
       return new Runnable(function* () {
         emit('a');
 
@@ -100,11 +131,39 @@ describe(Runnable.name, function() {
 
         emit('c');
 
-        return 'd';
+        return Promise.resolve('d');
       });
     }));
 
-    it('should cancel async task', marble('abc', async function(emit) {
+    it('should handle rejected promise within simple runnable', marble('ab#c', function(emit) {
+      return new Runnable(function* () {
+        emit('a');
+
+        yield 1;
+
+        emit('b');
+
+        yield Promise.reject('c');
+      });
+    }));
+
+    it('should allow rejected promise to be caught', marble('ab|d', function(emit) {
+      return new Runnable(function* () {
+        emit('a');
+
+        yield 1;
+
+        emit('b');
+
+        try {
+          yield Promise.reject('c');
+        } catch (error) {
+          return 'd';
+        }
+      });
+    }));
+
+    it('should allow interupt async task with return', marble('ab|e', function(emit) {
       const runnable = new Runnable(function* () {
         emit('a');
 
@@ -113,22 +172,91 @@ describe(Runnable.name, function() {
         try {
           yield forever();
         } finally {
-          if (Task.canceled) {
-            emit('c');
-          } else {
-            emit('d');
-          }
+          return 'e';
         }
       });
 
       const task = runnable.run();
 
-      await Promise.resolve();
+      setTimeout(function() {
+        task.interrupt();
+      });
 
-      task.cancel();
+      return task;
     }));
 
-    it('should allow chaining tasks', marble('ab12cd12ef', async function(emit) {
+    it('should allow interupt async task with throw', marble('ab#f', function(emit) {
+      const runnable = new Runnable(function* () {
+        emit('a');
+
+        emit('b');
+
+        try {
+          yield forever();
+        } finally {
+          throw 'f';
+        }
+      });
+
+      const task = runnable.run();
+
+      setTimeout(function() {
+        task.interrupt();
+      });
+
+      return task;
+    }));
+
+    it.skip('should allow interrupt async task with more yield');
+
+    it.only('should establish parent / child relationship during task construction', function() {
+      debugger;
+      const tasks = { children: [] };
+
+      tasks.parent = new Task({
+        next() {
+          const child = new Task({
+            next() {
+              return {
+                done: true,
+                value: forever()
+              };
+            }
+          });
+
+          tasks.children.push(child);
+
+          return child;
+        }
+      });
+
+      expect([ ...tasks.parent.children ][0]).to.equal(tasks.children[0]);
+    });
+
+    it.skip('should establish parent / child relationship through async operations', function() {
+      const tasks = { children: [] };
+
+      tasks.parent = new Task({
+        next() {
+          const child = new Task({
+            next() {
+              return {
+                done: false,
+                value: forever()
+              };
+            }
+          });
+
+          tasks.children.push(child);
+
+          return child;
+        }
+      });
+
+      expect([ ...tasks.parent.children ][0]).to.equal(tasks.children[0]);
+    });
+
+    it.skip('should allow chaining tasks', marble('ab12cd12ef', function(emit) {
       const child = new Runnable(function* () {
         emit('1');
 
@@ -160,7 +288,7 @@ describe(Runnable.name, function() {
       return parent.run();
     }));
 
-    it.skip('should allow parant cancel to cancel children', marble('ab12a', async function(emit) {
+    it.skip('should allow parant cancel to cancel children', marble('ab12a', function(emit) {
       const child = new Runnable(function* () {
         emit('1');
 
@@ -188,7 +316,7 @@ describe(Runnable.name, function() {
 
       const task = parent.run();
 
-      await Promise.resolve();
+      Promise.resolve();
 
       task.cancel();
     }));
